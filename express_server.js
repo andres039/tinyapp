d7dbd796f8afc785da4199913937e3b2ed668c7d
Andres <andres039039@hotmail.com>
2021-11-09 17:34:43 -0500

    Adds delete buttons and functionality

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3004;
-app.use(express.urlencoded());
+const PORT = 3005;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -18,19 +21,27 @@ function generateRandomString() {
   return randomArray.join("");
 }
 
-
 app.get("/urls", (req, res) => {
+  console.log('URL:', urlDatabase)
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", { urlDatabase });
+  res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString()
   urlDatabase[randomString] = req.body.longURL
-  res.redirect(`/urls/${randomString}`); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${randomString}`);
+});   
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -38,8 +49,9 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL]  
   res.redirect(longURL);
 });
 

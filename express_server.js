e51666dc54d083b7f3ceeb5a87325cbf2d7cc46d
Andres <andres039039@Hotmail.com>
2021-11-09 03:17:52 +0000

    implemented a random string generator

--- express_server.js
+++ express_server.js
@@ -1,24 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3004;
-
-app.set('view engine', 'ejs');
+app.use(express.urlencoded());
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
+function generateRandomString() {
+  const str = "0123456789abcdefghijklmnopqrstuvwxyz";
+  let randomArray = [];
+  for (let i = 0; i < 6; i++) {
+    randomArray.push(str[Math.floor(Math.random() * 35)]);
+  }
+  return randomArray.join("");
+}
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", {urlDatabase});
+  res.render("urls_index", { urlDatabase });
 });
-
-app.get('/urls/:shortURL', (req,res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
